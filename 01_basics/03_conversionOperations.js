ca65e1ca683a3a73534d3fa7937a6271755ac975
hellobubai <103349036+hellobubai@users.noreply.github.com>
2023-07-29 16:32:56 +0000

    Conversions of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+// let score = "33" // => 33 
+// let score = "33afa" // => NaN 
+// let score = null // => 0 
+// let score = undefined // => NaN
+// let score = true // => 1
+// let score = false // => 0
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// let isLoggedIn = "1" // => true
+// let isLoggedIn = "0" // => true
+// let isLoggedIn = "abc" // => true
+// let isLoggedIn = "" // => false
+// let isLoggedIn = 1 // => true
+// let isLoggedIn = 0 // => false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
