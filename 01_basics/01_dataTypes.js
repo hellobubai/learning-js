ecc1a984d503fe60f54eaeb82d92be781163aeb5
hellobubai <103349036+hellobubai@users.noreply.github.com>
2023-07-29 16:15:18 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log( 3 
+    +
+    3) // code readibility should be high
+
+console.log("Bubai")
+
+let name = "Bubai"
+let age = 23
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
